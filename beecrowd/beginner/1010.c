2a21063e9c24994b27101a4be7b9651360c51c7b
Muhammad Naim <naimahmed.srj@gmail.com>
2025-04-05 21:25:37 +0600

    *

---
+++ beecrowd/beginner/1010.c
@@ -0,0 +1,13 @@
+
+// 
+
+#include <stdio.h>
+
+int main() {
+    int code1, units1, code2, units2;
+    float price1, price2, total;
+    scanf("%d %d %f %d %d %f", &code1, &units1, &price1, &code2, &units2, &price2);
+    total = (units1 * price1) + (units2 * price2);
+    printf("VALOR A PAGAR: R$ %.2f\n", total);
+    return 0;
+}
