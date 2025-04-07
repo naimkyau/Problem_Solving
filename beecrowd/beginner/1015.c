81bffdb05a58c4cc010f81651cc4952d013b5a97
Muhammad Naim <naimahmed.srj@gmail.com>
2025-04-07 08:29:44 +0600

    *

---
+++ beecrowd/beginner/1015.c
@@ -0,0 +1,13 @@
+
+// Distance Between Two Points
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double x1, y1, x2, y2, distance;
+    scanf("%lf %lf %lf %lf", &x1, &y1, &x2, &y2);
+    distance = sqrt(((x1 - x2)*(x1 - x2))+((y1 - y2)*(y1 - y2)));
+    printf("%.4lf\n",distance);
+    return 0;
+}
