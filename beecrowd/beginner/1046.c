db43826b90084c688b820ecf7f595ad2ff14f9df
Muhammad Naim <naimahmed.srj@gmail.com>
2025-05-02 22:45:27 +0600

    *

---
+++ beecrowd/beginner/1046.c
@@ -0,0 +1,14 @@
+// Game Time
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    if (a >= b)
+    {
+        b += 24;
+    }
+    printf("O JOGO DUROU %d HORA(S)\n", b-a);
+    return 0;
+}
